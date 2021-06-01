cd5692453f2320e3a4ac14c1c322a3a2e7378c55
wsm970809 <2225788412@qq.com>
2021-06-01 20:15:41 +0800

    first commit

---
+++ src/components/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login_container">
+        <div class="login-box">
+            <div class="login_logo">
+                <img src="../assets/logo.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="less" scoped>
+    .login_container{
+        background-color: darkblue;
+        height: 100%;
+    }
+</style>
+<script>
+export default {
+    
+}
+</script>
